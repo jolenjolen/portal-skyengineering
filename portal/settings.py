b38c0972e4888088f142c66a1ef33161162bee59
jolenjolen <jolen@email.com>
2026-03-18 20:33:09 +0000

    create base for login to Sky Portal

--- portal/settings.py
+++ portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sodcdkm5wo%e!w&u&9gi84o0(xg30!wilrm$%z_#-96o^mcgld
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'DBv2',
     }
 }
 
