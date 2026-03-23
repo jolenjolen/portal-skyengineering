889ec686936f05ee7a1068880d8fd4ced5e1a342
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2026-03-23 10:31:21 +0000

    push

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
-# Main index page
 def index_view(request):
-    # Optional: check if user is logged in via session
-    if not request.session.get('user_id'):
-        from django.shortcuts import redirect
-        return redirect('login')  # redirect to accounts login if not logged in
     return render(request, 'dashboard/index.html')
