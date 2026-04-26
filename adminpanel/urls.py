b1b904ffb2d43ce92911b2e963660489704dbfab
Muhammed Hasan <muhammed.husam.hasan@gmail.com>
2026-04-26 18:19:01 +0100

    added manage user functions: change role, change status

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -9,5 +9,7 @@ from . import views
 urlpatterns =[
     path("",views.admin_dashboard,name="admin_dashboard"),
     path("users/",views.manage_users,name="manage_users"),
-    path("users/<int:user_id>/reset-password",views.reset_user_password,name="reset_user_password")
+    path("users/<int:user_id>/reset-password/",views.reset_user_password,name="reset_user_password"),
+    path("users/<int:user_id>/toggle-active/",views.toggle_user_active,name="toggle_user_active"),
+    path("users/<int:user_id>/change-role/",views.change_user_role,name="change_user_role"),
 ]
